c920776036c180fd6ffc0c39c7d81be77833b449
pankaj <pankajteceract@gmail.com>
2024-11-19 00:59:08 +0530

    Delete the protected folder from the app/platform because, we already have tested the authentication is working properly

--- app/(platform)/protected/page.tsx
+++
@@ -1,8 +0,0 @@
-const ProtectedPage = () => {
-  return (
-    <div className="h-full flex justify-center items-center">
-      This is Protected Page.
-    </div>
-  );
-};
-export default ProtectedPage;
